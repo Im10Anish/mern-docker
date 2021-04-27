b441fc5e45d9710c55a94fbc8de47dd447433c8a
anish kumar <anish.kumar@dibizglobal.com>
2021-04-27 15:12:05 +0530

    patch: added CRUD apis for blog post

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 //server.js
 const express = require('express')
+const bodyParser = require('body-parser')
+
+// Routes
+const postRouter = require('./src/routes/post.router')
+
 const app = express()
 const PORT = 8080
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(bodyParser.json())
+
+app.use('/posts', postRouter)
 
 // Our DB Configuration
 require('./src/database')
